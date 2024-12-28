a2109eb5b6f1b1aa50a69efe7f9357e55ebd3c59
PaulaEsteve <paulaeg78@gmail.com>
2024-12-28 19:41:37 +0100

    Crear un controlador desde el terminal

--- src/Controller/IniciController.php
+++ src/Controller/IniciController.php
@@ -1,6 +1,10 @@
 <?php
-namespace App\Controller; 
-use Symfony\Component\HttpFoundation\Response; 
+/*els subespais es separen amb \ 
+tots els espais de noms pengen d'una arrel App
+*/
+namespace App\Controller; // perteneix al espai de noms Controller ( cada carpeta dins de src es un espai de noms )
+use Symfony\Component\HttpFoundation\Response; ///var/www/html/symfony/retailPaula/vendor/symfony/http-foundation/Response.php
+//use Symfony\Component\HttpFoundation\Response as Res; per a cridar a la clase de forma abreujada
 use Symfony\Component\Routing\Annotation\Route;
 class IniciController {
     #[Route('/' ,name:'inici')]
